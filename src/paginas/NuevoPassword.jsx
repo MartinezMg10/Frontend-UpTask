3b0d76cb90c8d081ffa243da833486e870e5915d
MartinezMg <garcia.miguelangel1206@gmail.com>
2023-04-07 22:36:03 -0500

    Estilos de Nuevo password

--- src/paginas/NuevoPassword.jsx
+++ src/paginas/NuevoPassword.jsx
@@ -1,9 +1,27 @@
 
 const NuevoPassword = () => {
   return (
-    <div>
-      <h1>Nuevo Password</h1>
-    </div>
+    <>
+    <h1 className="text-sky-600 font-black text-6xl capitalize">Restablece tu Password y no pierdas acceso a tus <span className="text-slate-700">proyectos</span></h1>
+
+    <form className="my-10 bg-white shadow rounded-lg p-10">
+        <div className="my-5">
+          <label className="uppercase text-gray-600 block text-xl font-bold"
+          htmlFor="password">Nuevo Password</label>
+          <input
+          id="Password"
+          type="password"
+          placeholder="Escribe tu nuevo password"
+          className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+        <input
+          type="submit"
+          value="Guardar nuevo password"
+          className="bg-sky-700 w-full py-3 mb-5 text-white uppercase font-bold rounded  hover:cursor-pointer hover:bg-sky-800 transition-colors"
+        />
+    </form>
+  </>
   )
 }
 
