36fe5099532266ede5156de68bdf5cdacb3e036a
MartinezMg <garcia.miguelangel1206@gmail.com>
2023-04-07 21:59:47 -0500

    estilos de registrar

--- src/paginas/Registrar.jsx
+++ src/paginas/Registrar.jsx
@@ -38,10 +38,19 @@ const Registrar = () => {
           className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
           />
         </div>
-
+        <div className="my-5">
+          <label className="uppercase text-gray-600 block text-xl font-bold"
+          htmlFor="password2">Repetir Password</label>
+          <input
+          id="Password2"
+          type="password"
+          placeholder="Repetir tu password"
+          className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
         <input
           type="submit"
-          value="Iniciar Sesion"
+          value="Crear Cuenta"
           className="bg-sky-700 w-full py-3 mb-5 text-white uppercase font-bold rounded  hover:cursor-pointer hover:bg-sky-800 transition-colors"
         />
     </form>
@@ -49,7 +58,7 @@ const Registrar = () => {
     <nav className="lg:flex lg:justify-between">
       <Link
       className="block text-center my-5 text-slate-500 uppercase text-sm"
-      to="registrar">¿No tienes una cuenta? Registrate</Link>
+      to="/">¿Ya tienes una cuenta? Inicia sesion</Link>
 
       <Link
       className="block text-center my-5 text-slate-500 uppercase text-sm"
